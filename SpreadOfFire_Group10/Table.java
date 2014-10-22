8301eb87472e8436d4ccd72c1bfc139fd728b2ed
AwikaP <awk-pang@hotmail.com>
2014-10-22 22:01:03 +0700

    Update Table.java

--- SpreadOfFire_Group10/Table.java
+++ SpreadOfFire_Group10/Table.java
@@ -1,27 +1,74 @@
-
-
-package gridtable;
 import java.awt.*;
 import javax.swing.*;
+/**
+ * Write a description of class TestGrid here.
+ * 
+ * @author (Project Group 10)
+ * @version (21/10/2014)
+ */
+public class Grid extends JPanel
+{
+    private Cell cell [][] ;
+    private int cellSize;
+    private boolean showVal;
 
-public class Table extends JPanel{
-   private int cellSize;
-   private int cellNum; //จำนวนcell
-   private int a[][];
-   
-   public Table(){
-       a= new int[20][20];
-       cellSize = 20;
-       cellNum = 31;     
-   }
-   
-   public void paintComponent(Graphics g){ 
+    public Grid(){        
+        cellSize = 15;
+        setLayout(new FlowLayout(5, 650, 4));
+        showVal = false;
+    }
+
+    public void Update(Cell cell[][]){
+        this.cell = cell;
+        repaint();
+    }
+
+    public void paintComponent(Graphics g){
         super.paintComponent(g);
-        for(int i=0; i<a.length-1; i++){
-            for(int j=0 ; j<a.length-1; j++){
-             g.fillRect((i+1)*cellSize,(j+1)*cellSize,cellSize-2,cellSize-2);
-            
+        for(int i = 0 ; i< cell.length;i++){
+            for(int j = 0; j < cell.length ; j++ ){
+                if(cell[i][j]!=null){
+                    int x = (i+1)*20;
+                    int y = (j+1)*20;
+                    if(cell[i][j].getState() == Cell.EMPTY){
+                        g.setColor(Color.YELLOW);
+                    }
+                    else if(cell[i][j].getState() == Cell.TREE){
+                        g.setColor(Color.GREEN);
+                    }
+                    else{
+                        g.setColor(Color.RED);
+                    }
+                    g.fillRect(x,y,20,20);
+                    g.setColor(Color.BLACK);
+                    if(showVal){
+
+                        if (cell[i][j].getState() == Cell.EMPTY) {
+                            g.drawString("0" , x+7 , y+14  );
+                        } else if (cell[i][j].getState() == Cell.TREE) {
+                            g.drawString("1" , x+7 , y+14 );
+                        } else {
+                            g.drawString("2" , x+7 , y+14 );
+                        }
+
+                        g.drawRect(x , y, 20, 20);
+                    }
+
+                }
+            }
         }
-      }   
-   }
+    }
+
+    public int getcellSize(){
+        return cellSize;
+    }
+
+    public void setcellSize(int cellSize){
+        this.cellSize = cellSize;
+    }
+
+    public void setShowVal(boolean showVal) {
+        this.showVal = showVal;
+    }
+
 }
