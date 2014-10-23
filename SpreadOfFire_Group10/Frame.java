4d06db5da270bfc8b10ca82f4008ea1fe3dfef2e
mpanrap <mpanrap@yahoo.ca>
2014-10-23 16:00:45 +0700

    Frame.java
    update JButton

--- SpreadOfFire_Group10/Frame.java
+++ SpreadOfFire_Group10/Frame.java
@@ -9,16 +9,20 @@ import javax.swing.*;
 public class Frame
 {
     JFrame frame;
+    Controller c;
     Simulation simulation;
     Grid myGrid;
     public Frame(){
-        frame = new JFrame("Spreading Fire");        
+        frame = new JFrame("Spreading Fire");  
         frame.setSize(800,600);
         myGrid = new Grid();
+        //frame.setLayout(new GridLayout(1,2));
         simulation = new Simulation(myGrid, 25, 1.0, 1.0, 0.0);
-        frame.add(myGrid);
+        c = new Controller(simulation,myGrid);
+        frame.add(myGrid); 
         myGrid.setShowVal(true);
         frame.setVisible(true);
+        
     }
     
     
